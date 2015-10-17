205b32894377067eb4c64bc53e85898639ad783f
Russell Krupke <russell@sa-mp.nl>
2015-10-17 21:49:32 +0100

    Support /help in JavaScript. Remove the commands from Pawn.

--- javascript/features/introduction/introduction.js
+++ javascript/features/introduction/introduction.js
@@ -3,15 +3,16 @@
 // be found in the LICENSE file.
 
 let Feature = require('components/feature_manager/feature.js'),
+    Menu = require('components/menu/menu.js'),
     Message = require('components/dialogs/message.js');
 
 // Files in which the JSON data for the /irc and /rules command is stored. The contents must be an
 // array with one or more strings as its contents, each of which will be a line in the dialog.
-const DATA_FILE_IRC = 'data/irc_command.json';
-const DATA_FILE_RULES = 'data/rules_command.json';
+const DATA_FILE_IRC = 'data/commands/irc.json';
+const DATA_FILE_RULES = 'data/commands/rules.json';
 
 // File in which the JSON data for the /help command is stored.
-const DATA_FILE_HELP = 'data/help_command.json';
+const DATA_FILE_HELP = 'data/commands/help.json';
 
 // The Introduction feature provides commands and functionality to help new players get familiar
 // with Las Venturas Playground. Among this is the /help command.
@@ -19,16 +20,32 @@ class Introduction extends Feature {
   constructor(playground) {
     super(playground);
 
+    this.helpMenu_ = this.createMenuFromExternalData(DATA_FILE_HELP, 'What can we help you with?');
+
     this.ircMessage_ = this.createMessageFromExternalData(DATA_FILE_IRC);
     this.rulesMessage_ = this.createMessageFromExternalData(DATA_FILE_RULES);
-
-    // TODO(Russell): Load /help information from a data file.
 
     // TODO(Russell): Include /commands in this.
 
     playground.commandManager.registerCommand('help', Introduction.prototype.onHelpCommand.bind(this))
                              .registerCommand('irc', Introduction.prototype.onIrcCommand.bind(this))
                              .registerCommand('rules', Introduction.prototype.onRulesCommand.bind(this));
+  }
+
+  // Loads |filename| as a JSON object and use it to compile a new Menu instance with the available
+  // options. Selecting one of these options will open a dialog with the associated data.
+  createMenuFromExternalData(filename, title) {
+    let menuData = JSON.parse(readFile(filename)),
+        menu = new Menu(title);
+
+    Object.keys(menuData).forEach(topic => {
+      let topicMessage = new Message(menuData[topic].join('\n'));
+
+      // Automagically display the |topicMessage| when the player selects the topic.
+      menu.addItem(topic, player => topicMessage.displayForPlayer(player));
+    });
+
+    return menu;
   }
 
   // Loads |filename| as a JSON array having one or more strings as its contents, and creates a new
@@ -44,7 +61,7 @@ class Introduction extends Feature {
   // Called when a player executes the /help command in-game. A dialog will be displayed that allows
   // them to access lots of introductory information about our community.
   onHelpCommand(player, parameters) {
-    // TODO(Russell): Display the actual /help contents to the player.
+    this.helpMenu_.displayForPlayer(player);
   }
 
   // Called when a player wants to know more about our IRC channel. A message will be shown with the

1ad92c67fe0b28ddd1fa8b5497fd8236a7a30027
Russell Krupke <russell@sa-mp.nl>
2015-10-17 21:40:45 +0100

    Support the /irc command in JavaScript

--- javascript/features/introduction/introduction.js
+++ javascript/features/introduction/introduction.js
@@ -5,8 +5,9 @@
 let Feature = require('components/feature_manager/feature.js'),
     Message = require('components/dialogs/message.js');
 
-// File in which the JSON data for the /rules command is stored. The contents must be an array with
-// one or more strings as its contents, each of which will be a line in the dialog.
+// Files in which the JSON data for the /irc and /rules command is stored. The contents must be an
+// array with one or more strings as its contents, each of which will be a line in the dialog.
+const DATA_FILE_IRC = 'data/irc_command.json';
 const DATA_FILE_RULES = 'data/rules_command.json';
 
 // File in which the JSON data for the /help command is stored.
@@ -18,14 +19,15 @@ class Introduction extends Feature {
   constructor(playground) {
     super(playground);
 
+    this.ircMessage_ = this.createMessageFromExternalData(DATA_FILE_IRC);
     this.rulesMessage_ = this.createMessageFromExternalData(DATA_FILE_RULES);
 
     // TODO(Russell): Load /help information from a data file.
 
     // TODO(Russell): Include /commands in this.
-    // TODO(Russell): Include /irc in this.
 
     playground.commandManager.registerCommand('help', Introduction.prototype.onHelpCommand.bind(this))
+                             .registerCommand('irc', Introduction.prototype.onIrcCommand.bind(this))
                              .registerCommand('rules', Introduction.prototype.onRulesCommand.bind(this));
   }
 
@@ -41,16 +43,18 @@ class Introduction extends Feature {
 
   // Called when a player executes the /help command in-game. A dialog will be displayed that allows
   // them to access lots of introductory information about our community.
-  //
-  // @command /help
   onHelpCommand(player, parameters) {
     // TODO(Russell): Display the actual /help contents to the player.
   }
 
+  // Called when a player wants to know more about our IRC channel. A message will be shown with the
+  // information they need to connect and join the conversation.
+  onIrcCommand(player, parameters) {
+    this.ircMessage_.displayForPlayer(player);
+  }
+
   // Called when a player executes the /rules command in-game. A dialog will be shown with all the
   // rules that they will have to keep in mind while enjoying some time on the server.
-  //
-  // @command /rules
   onRulesCommand(player, parameters) {
     this.rulesMessage_.displayForPlayer(player);
   }

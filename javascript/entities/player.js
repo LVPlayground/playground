05ece63fb9969c1dcaf6d8659d171430c1915796
Russell Krupke <russell@sa-mp.nl>
2015-10-24 19:34:03 +0100

    Enable creation fake players for the purposes of testing

--- javascript/entities/player.js
+++ javascript/entities/player.js
@@ -2,6 +2,9 @@
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
 
+// Identifier, stored as an IP address, that can be used to detect players created for testing.
+const TEST_PLAYER_IDENTIFIER = '0.0.0.0';
+
 // See https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Symbol
 let playerCreateSymbol = Symbol('Private symbol to limit creation of Player instances.'),
     players = {};
@@ -41,6 +44,36 @@ class Player {
 
   // Returns the level of this player. Synchronized with the gamemode using the `levelchange` event.
   get level() { return this.level_; }
+
+  // -----------------------------------------------------------------------------------------------
+  // The following methods are only meant for testing!
+
+  // Simulates connecting of a player optionally identified by id |name| for the purposes of tests.
+  // Make sure to also call |destroyForTest| after the test is complete to remove the player again.
+  static createForTest(playerId, nickname) {
+    playerId = playerId || 0;
+
+    if (players.hasOwnProperty(playerId))
+      throw new Error('Unable to create a player for testing purposes, id ' + playerId + ' already taken.');
+
+    players[playerId] = new Player(playerCreateSymbol, playerId);
+    players[playerId].name_ = nickname || 'TestPlayer';
+    players[playerId].ipAddress_ = TEST_PLAYER_IDENTIFIER;
+
+    return players[playerId];
+  }
+
+  // Destroys the player instance of |playerId| for the purposes of testing. The associated player
+  // must have been created by a test as well, otherwise an exception will be thrown.
+  static destroyForTest(player) {
+    if (!players.hasOwnProperty(player.id))
+      throw new Error('No player with this id has connected to the server.');
+
+    if (player.ipAddress != TEST_PLAYER_IDENTIFIER)
+      throw new Error('The player with this id was not created by a test.');
+
+    delete players[player.id];
+  }
 };
 
 // The level of a  player. Can be accessed using the `level` property on a Player instance.
@@ -51,7 +84,7 @@ Player.LEVEL_MANAGEMENT = 2;
 // Called when a player connects to Las Venturas Playground. Registers the player as being in-game
 // and initializes the Player instance for them.
 self.addEventListener('playerconnect', event =>
-  players[event.playerid] = new Player(playerCreateSymbol, event.playerid));
+    players[event.playerid] = new Player(playerCreateSymbol, event.playerid));
 
 // Called when the level of a player changes. This event is custom to Las Venturas Playground.
 self.addEventListener('playerlevelchange', event => {
@@ -73,7 +106,7 @@ self.addEventListener('playerlevelchange', event => {
 
 // Called when a player disconnects from the server. Removes the player from our registry.
 self.addEventListener('playerdisconnect', event =>
-  delete players[event.playerid]);
+    delete players[event.playerid]);
 
 // Expose the Player object globally since it will be commonly used.
 global.Player = Player;

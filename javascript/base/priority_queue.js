e4d10eb3c3b5fce1ac3106065a5508ace8cddab3
Russell Krupke <russell@sa-mp.nl>
2015-10-24 01:28:42 +0100

    Add tests for the PriorityQueue

--- javascript/base/priority_queue.js
+++ javascript/base/priority_queue.js
@@ -7,41 +7,44 @@
 // dequeue entries. Additionally, existing entries can be filtered using a predicate.
 class PriorityQueue {
   constructor(comperator) {
-    this.comperator = comperator;
-    this.entries = [];
+    this.comperator_ = comperator;
+    this.entries_ = [];
   }
 
-  // Enqueues |entry| in the priority queue.
-  enqueue(entry) {
-    this.entries.push(entry);
-    this.bubble(this.entries.length - 1);
+  // Enqueues |entries| in the priority queue. Multiple entries may be enqueued at once by passing
+  // multiple arguments to this method.
+  enqueue(...entries) {
+    entries.forEach(entry => {
+      this.entries_.push(entry);
+      this.bubble(this.entries_.length - 1);
+    });
   }
 
   // Removes the element with the highest priority. Throws if the priority queue is empty.
   dequeue() {
-    if (!this.entries.length)
+    if (!this.entries_.length)
       throw new Error('Cannot dequeue from an empty PriorityQueue.')
 
-    this.entries.shift();
+    this.entries_.shift();
   }
 
   // Returns the entry with the highest priority. Throws if the priority queue is empty.
   peek() {
-    if (!this.entries.length)
+    if (!this.entries_.length)
       throw new Error('Cannot peek in an empty PriorityQueue.');
 
-    return this.entries[0];
+    return this.entries_[0];
   }
 
   // Filters items in m the priority queue for which |predicate| returns false. This method will
   // iterate over all entries in the queue, and therefore has O(n) performance.
   filter(predicate) {
-    this.entries = this.entries.filter(predicate);
+    this.entries_ = this.entries_.filter(predicate);
   }
 
   // Returns the number of entries currently in this priority queue.
   size() {
-    return this.entries.length;
+    return this.entries_.length;
   }
 
   // Logarithmically bubbles the element at |index| up to the position where it should be. Used
@@ -59,14 +62,14 @@ class PriorityQueue {
 
   // Compares the entries at indices |lhs| and |rhs| with each other.
   compare(lhs, rhs) {
-    return this.comperator(this.entries[lhs], this.entries[rhs]);
+    return this.comperator_(this.entries_[lhs], this.entries_[rhs]);
   }
 
   // Swaps the entries at indices |lhs| and |rhs| with each other.
   swap(lhs, rhs) {
-    let temp = this.entries[lhs];
-    this.entries[lhs] = this.entries[rhs];
-    this.entries[rhs] = temp;
+    let temp = this.entries_[lhs];
+    this.entries_[lhs] = this.entries_[rhs];
+    this.entries_[rhs] = temp;
   }
 
 };

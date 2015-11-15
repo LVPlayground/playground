d53cd73c8306f8f70b30a10b2c196b40d67327da
Russell Krupke <russell@sa-mp.nl>
2015-11-15 00:44:34 +0000

    Score board: split out an AbsoluteTimeView to get proportional display
    w/o using the text draw proportionality flag, because it's super ugly

--- javascript/features/races/score_board.js
+++ javascript/features/races/score_board.js
@@ -9,7 +9,7 @@ let Rectangle = require('components/text_draw/rectangle.js'),
 const BACKGROUND_COLOR = new Color(0, 0, 0, 100);
 
 // Color of the text indicating the number of players. Should be white-ish.
-const PLAYER_COUNT_COLOR = new Color(255, 255, 255, 120);
+const PLAYER_COUNT_COLOR = new Color(255, 255, 255, 100);
 
 // Powers the visual score board on the right-hand side of a player's screen. It displays data about
 // the current race, for example the time and distances to the other player, but also time based on
@@ -18,10 +18,13 @@ const PLAYER_COUNT_COLOR = new Color(255, 255, 255, 120);
 // The ScoreBoard class implements the following user interface:
 //
 //     =============================
-//     =     __                    =
-//     =  ## |  |       00:00.000  =
-//     =  ## |__|  PR: +00:00.000  =
+//     =   _                       =
+//     =  |  | TH       00:00.000  =
+//     =  |__| /0    PR 00:00.000  =
 //     =                           =
+//     =============================
+//
+//     =============================
 //     =                           =
 //     =  #1 FirstRacerName        =
 //     =                  -00.000  =
@@ -51,7 +54,8 @@ class ScoreBoard {
 
     this.positionBackground_ = new Rectangle(500, 140, 106, 36.8, BACKGROUND_COLOR);
 
-    // Section (1): Position, current time and difference from the player's personal best.
+    // Section (1): Position compared to the other players in the current race
+    // ---------------------------------------------------------------------------------------------
 
     this.positionValue_ = new TextDraw({
       position: [505, 143],
@@ -64,7 +68,7 @@ class ScoreBoard {
     });
 
     this.positionSuffix_ = new TextDraw({
-      position: [516, 145.225],
+      position: [516, 145],
 
       text: 'st',
       font: TextDraw.FONT_MONOSPACE,
@@ -73,7 +77,7 @@ class ScoreBoard {
     });
 
     this.participantsValue_ = new TextDraw({
-      position: [524.73, 153.796],
+      position: [524.9, 153.796],
       color: PLAYER_COUNT_COLOR,
 
       text: '_',  // to be filled in on start
@@ -84,36 +88,21 @@ class ScoreBoard {
       proportional: false
     });
 
-    this.timeValue_ = new TextDraw({
-      position: [556.5, 145.5],
 
-      text: '00:00.000',
-      font: TextDraw.FONT_PRICEDOWN,
-      letterSize: [0.263, 1.036],
-      shadowSize: 0
-    });
+    // Section (2): Running time in the current race, personal record and relative offset of that.
+    // ---------------------------------------------------------------------------------------------
 
-    // TODO: Only display the personal record data when available.
+    this.timeValue_ = new AbsoluteTimeView(556.5, 145.5);
+
+    this.personalRecordValue_ = new AbsoluteTimeView(556.5, 154.726);
     this.personalRecordLabel_ = new TextDraw({
-      position: [538, 153.6],
+      position: [543, 153.5],
 
       text: 'pr',
       font: TextDraw.FONT_MONOSPACE,
       letterSize: [0.205, 1.235],
       shadowSize: 0
     });
-
-    this.personalRecordValue_ = new TextDraw({
-      position: [599.9, 154.726],
-
-      text: '00:00.000',
-      font: TextDraw.FONT_PRICEDOWN,
-      letterSize: [0.263, 1.036],
-      shadowSize: 0,
-      alignment: TextDraw.ALIGN_RIGHT
-    });
-
-    // TODO: Display the current ranks and distances between the players.
   }
 
   // Displays the score board for the player. All (initial) values for the texts should've been set,
@@ -153,7 +142,7 @@ class ScoreBoard {
   // the score board until relative times based on their performance are known.
   setBestTime(time) {
     this.hasPersonalRecords_ = true;
-    this.personalRecordValue_.text = ScoreBoard.formatTime(time, true);
+    this.personalRecordValue_.setTime(this.player_, time);
 
     if (this.displaying_) {
       this.personalRecordLabel_.displayForPlayer(this.player_);
@@ -164,8 +153,7 @@ class ScoreBoard {
   // Called every ~hundred milliseconds while the race is active. Only update the high-resolution
   // race-duration counter on the player's screen.
   update(currentTime) {
-    this.timeValue_.updateTextForPlayer(
-        this.player_, ScoreBoard.formatTime(currentTime - this.participant_.startTime, true));
+    this.timeValue_.setTime(this.player_, currentTime - this.participant_.startTime);
   }
 
   // -----------------------------------------------------------------------------------------------
@@ -193,4 +181,106 @@ class ScoreBoard {
   }
 };
 
+// The TimeView is a view that can be used to draw time in the format of [00:00.000] with consistent
+// spacing regardless of the value and without having to rely on ugly proportional text rendering.
+class AbsoluteTimeView {
+  constructor(x, y) {
+    this.minuteValue_ = '00';
+    this.minuteView_ = new TextDraw({
+      position: [x + 5.4, y],
+
+      text: this.minuteValue_,
+      font: TextDraw.FONT_PRICEDOWN,
+      alignment: TextDraw.ALIGN_CENTER,
+      letterSize: [0.272, 1.007],
+      shadowSize: 0
+    });
+
+    this.secondValue_ = '00';
+    this.secondView_ = new TextDraw({
+      position: [x + 14.033 + 5.4, y],
+
+      text: this.secondValue_,
+      font: TextDraw.FONT_PRICEDOWN,
+      alignment: TextDraw.ALIGN_CENTER,
+      letterSize: [0.272, 1.007],
+      shadowSize: 0
+    });
+
+    this.millisecondValue_ = '000';
+    this.millisecondView_ = new TextDraw({
+      position: [x + 26.8, y],
+
+      text: this.millisecondValue_,
+      font: TextDraw.FONT_PRICEDOWN,
+      letterSize: [0.272, 1.007],
+      shadowSize: 0
+    });
+
+    this.separatorView_ = new TextDraw({
+      position: [x + 10.8, y - 0.15],
+
+      text: ':____.',
+      font: TextDraw.FONT_PRICEDOWN,
+      letterSize: [0.185, 1.007],
+      shadowSize: 0
+    });
+
+    this.displaying_ = false;
+  }
+
+  setTime(player, time) {
+    let decimalSeconds = time / 1000;
+
+    let seconds = Math.floor(decimalSeconds);
+    let milliseconds = Math.floor((decimalSeconds - seconds) * 1000);
+
+    let minutes = Math.floor(seconds / 60);
+    if (minutes > 0)
+      seconds -= minutes * 60;
+
+    let minuteValue = ((minutes < 10) ? '0' : '') + minutes,
+        secondValue = ((seconds < 10) ? '0' : '') + seconds,
+        millisecondValue = (milliseconds < 100 ? (milliseconds < 10 ? '00' : '0') : '') + milliseconds;
+
+    // Update the local values if the absolute time view isn't being displayed yet, because there is
+    // nothing to update for the player(s) it is being shown to.
+    if (!this.displaying_) {
+      this.minuteView_.text = this.minuteValue_ = minuteValue;
+      this.secondView_.text = this.secondValue_ = secondValue;
+      this.millisecondView_.text = this.millisecondValue_ = millisecondValue;
+      return;
+    }
+
+    // Alternatively, update the live views that are being presented to the player.
+    if (this.minuteValue_ != minuteValue)
+      this.minuteView_.updateTextForPlayer(player, this.minuteValue_ = minuteValue);
+
+    if (this.secondValue_ != secondValue)
+      this.secondView_.updateTextForPlayer(player, this.secondValue_ = secondValue);
+
+    if (this.millisecondValue_ != millisecondValue)
+      this.millisecondView_.updateTextForPlayer(player, this.millisecondValue_ = millisecondValue);
+  }
+
+  displayForPlayer(player) {
+    this.minuteView_.displayForPlayer(player);
+    this.secondView_.displayForPlayer(player);
+    this.millisecondView_.displayForPlayer(player);
+    this.separatorView_.displayForPlayer(player);
+
+    this.displaying_ = true;
+  }
+
+  hideForPlayer(player) {
+    this.displaying_ = false;
+
+    this.separatorView_.hideForPlayer(player);
+    this.millisecondView_.hideForPlayer(player);
+    this.secondView_.hideForPlayer(player);
+    this.minuteView_.hideForPlayer(player);
+  }
+};
+
+// Only the ScoreBoard class is public, the other views are private to the implementation.
 exports = ScoreBoard;

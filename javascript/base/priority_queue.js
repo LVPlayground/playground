03e2cce34983b8145cac7418df31e9320d245a37
Russell Krupke <russell@sa-mp.nl>
2016-02-19 23:19:12 +0000

    Add a priority queue implementation in //base

---
+++ javascript/base/priority_queue.js
@@ -0,0 +1,50 @@
+// Copyright 2016 Las Venturas Playground. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+// Implementation of a simple yet fast priority queue. A |comparator| must be given in the class'
+// constructor that will be used for ordering on insertion. 
+class PriorityQueue {
+  constructor(comparator) {
+    this.comparator_ = comparator;
+    this.items_ = [];
+  }
+
+  // Pushes |items| in the priority queue.
+  push(...items) {
+    items.forEach(item => {
+      this.items_.push(item);
+      this.prioritize();
+    });
+  }
+
+  // Removes the top-most item from the priority queue. Returns the previously top-most item from
+  // the queue, or NULL in case the queue was empty.
+  pop() {
+    return this.items_.shift();
+  }
+
+  // Returns the number of items that have been added to this queue.
+  size() {
+    return this.items_.length;
+  }
+
+  // Prioritizes the newly added element appropriately per the comparator that applies to this
+  // priority queue. This uses an algorithm of logarithmic complexity.
+  prioritize() {
+    let index = this.items_.length - 1;
+    while (index >= 1) {
+      const candidateIndex = Math.floor((index - 1) / 2);
+      if (this.comparator_(this.items_[index], this.items_[candidateIndex]) > 0)
+        break;
+
+      const temp = this.items_[index];
+      this.items_[index] = this.items_[candidateIndex];
+      this.items_[candidateIndex] = temp;
+
+      index = candidateIndex;
+    }
+  }
+};
+
+exports = PriorityQueue;
---
+++ javascript/base/priority_queue.test.js
@@ -0,0 +1,47 @@
+// Copyright 2016 Las Venturas Playground. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+const PriorityQueue = require('base/priority_queue.js');
+
+describe('PriorityQueue', it => {
+  it('should behave like a regular queue with a void comparator', assert => {
+    const queue = new PriorityQueue((lhs, rhs) => 1),
+          values = [5, 10, 2, 1];
+
+    values.forEach(value => queue.push(value));
+    values.forEach(value => assert.equal(queue.pop(), value));
+  });
+
+  it('should prioritize the items with a ascending comparator', assert => {
+    const queue = new PriorityQueue((lhs, rhs) => lhs > rhs);
+
+    [5, 10, 2, 1].forEach(value => queue.push(value));
+    [1, 2, 5, 10].forEach(expected => assert.equal(queue.pop(), expected));
+  });
+
+  it('should prioritize the items with a descending comparator', assert => {
+    const queue = new PriorityQueue((lhs, rhs) => lhs < rhs);
+
+    [5, 10, 2, 1].forEach(value => queue.push(value));
+    [10, 5, 2, 1].forEach(expected => assert.equal(queue.pop(), expected));
+  });
+
+  it('should prioritize items when inserted simultaneously', assert => {
+    const queue = new PriorityQueue((lhs, rhs) => lhs > rhs);
+    queue.push(5, 10, 2, 1);
+
+    console.log(queue.items_);
+
+    [1, 2, 5, 10].forEach(expected => assert.equal(queue.pop(), expected));
+  });
+
+  it('should be able to count the size of its queue', assert => {
+    const queue = new PriorityQueue((lhs, rhs) => 1);
+
+    for (let i = 0; i < 100; ++i)
+      queue.push(i);
+
+    assert.equal(queue.size(), 100);
+  });
+});

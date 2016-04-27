f85d410eb14e39f8e39de4d69518525d5bda052f
Russell Krupke <russell@sa-mp.nl>
2016-04-27 01:49:18 +0100

    Reformat main.js </day>

--- javascript/main.js
+++ javascript/main.js
@@ -1,43 +1,46 @@
-// Copyright 2015 Las Venturas Playground. All rights reserved.
+// Copyright 2016 Las Venturas Playground. All rights reserved.
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
 
+const Server = require('server.js');
+const TestRunner = require('base/test/test_runner.js');
+
+// Import global objects.
 require('base/color.js');
 require('base/message.js');
 
+// Import the entities. These are global objects.
 require('entities/game_object.js');
 require('entities/player.js');
 require('entities/vehicle.js');
 require('entities/virtual_world.js');
 
-const Server = require('server.js');
-const TestRunner = require('base/test/test_runner.js');
-
 // All tests included in the JavaScript version of Las Venturas Playground must pass before we allow
 // the gamemode to be started. Without this requirement, certain features may break unexpectedly.
-let testRunner = new TestRunner();
-testRunner.run('**/*.test.js').then(() => {
-  // All tests passed, start the gamemode by instantiating the Playground object.
-  console.log('Passed all ' + testRunner.testCount + ' tests!');
+const testRunner = new TestRunner();
 
-  server = new Server();
-  server.featureManager.load({
-    account:       require('features/account/account_feature.js'),
-    activityLog:   require('features/activity_log/activity_log.js'),
-    commands:      require('features/commands/commands_feature.js'),
-    deathFeed:     require('features/death_feed/death_feed_feature.js'),
-    debug:         require('features/debug/debug_feature.js'),
-    friends:       require('features/friends/friends_feature.js'),
-    gangs:         require('features/gangs/feature.js'),
-    races:         require('features/races/race_feature.js')
-  });
+testRunner.run('**/*.test.js').then(() => {
+    console.log('Passed all ' + testRunner.testCount + ' tests!');
+
+    server = new Server();
+    server.featureManager.load({
+        account:       require('features/account/account_feature.js'),
+        activityLog:   require('features/activity_log/activity_log.js'),
+        commands:      require('features/commands/commands_feature.js'),
+        deathFeed:     require('features/death_feed/death_feed_feature.js'),
+        debug:         require('features/debug/debug_feature.js'),
+        friends:       require('features/friends/friends_feature.js'),
+        gangs:         require('features/gangs/feature.js'),
+        races:         require('features/races/race_feature.js')
+    });
 
 }, failures => {
-  // One or more tests have failed. Refuse to start the gamemode.
-  console.log('===============================================================');
-  console.log('There were one or more test failures. Please fix them before starting the server!\n');
-  failures.forEach(failure =>
-      console.log('  ' + failure.toString() + '\n'));
+    // One or more tests have failed. Refuse to start the gamemode.
+    console.log('===============================================================');
+    console.log('There were one or more test failures. Please fix them before starting the server!\n');
 
-  console.log('===============================================================\n');
+    failures.forEach(failure =>
+        console.log('  ' + failure.toString() + '\n'));
+
+    console.log('===============================================================\n');
 });

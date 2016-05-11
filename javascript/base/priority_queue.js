2b860b69db97de5164c11dcdf2fe7ff2fa9301ec
Russell Krupke <russell@sa-mp.nl>
2016-05-12 00:27:31 +0100

    Rewrite the Priority Queue implementation, add delete(), add tests

--- javascript/base/priority_queue.js
+++ javascript/base/priority_queue.js
@@ -2,54 +2,84 @@
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
 
-// Implementation of a simple yet fast priority queue. A |comparator| must be given in the class'
-// constructor that will be used for ordering on insertion. 
+// Implementation of a simple yet fast priority queue. A |comparator| must be given when
+// constructing the priority queue that will be used for ordering items.
 class PriorityQueue {
-  constructor(comparator) {
-    this.comparator_ = comparator;
-    this.items_ = [];
-  }
+    constructor(comparator) {
+        if (typeof comparator !== 'function')
+            throw new Error('The comparator passed to a PriorityQueue must be a function.');
 
-  // Pushes |items| in the priority queue.
-  push(...items) {
-    items.forEach(item => {
-      this.items_.push(item);
-      this.prioritize();
-    });
-  }
-
-  // Returns the top-most item from the priority queue, or NULL if the queue is empty.
-  peek() {
-    return this.items_[0];
-  }
-
-  // Removes the top-most item from the priority queue. Returns the previously top-most item from
-  // the queue, or NULL in case the queue is empty.
-  pop() {
-    return this.items_.shift();
-  }
-
-  // Returns the number of items that have been added to this queue.
-  size() {
-    return this.items_.length;
-  }
-
-  // Prioritizes the newly added element appropriately per the comparator that applies to this
-  // priority queue. This uses an algorithm of logarithmic complexity.
-  prioritize() {
-    let index = this.items_.length - 1;
-    while (index >= 1) {
-      const candidateIndex = Math.floor((index - 1) / 2);
-      if (this.comparator_(this.items_[index], this.items_[candidateIndex]) > 0)
-        break;
-
-      const temp = this.items_[index];
-      this.items_[index] = this.items_[candidateIndex];
-      this.items_[candidateIndex] = temp;
-
-      index = candidateIndex;
+        this.comparator_ = comparator;
+        this.values_ = [];
     }
-  }
-};
+
+    // Returns whether the priority queue is empty.
+    isEmpty() { return !this.values_.length; }
+
+    // Gets the number of values stored in this priority queue.
+    get sizeNew() { return this.values_.length; }
+
+    // Pushes |values| in the priority queue. A binary search will be used to identify the location
+    // in the queue to insert the value.
+    push(...values) {
+        values.forEach(value => {
+            let insertionIndex = 0;
+            let boundary = this.values_.length;
+
+            while (insertionIndex < boundary) {
+                const candidateIndex = (insertionIndex + boundary) >>> 1;
+                if (this.comparator_(value, this.values_[candidateIndex]) < 0)
+                    insertionIndex = candidateIndex + 1;
+                else
+                    boundary = candidateIndex;
+            }
+
+            this.values_.splice(insertionIndex, 0, value);
+        });
+    }
+
+    // Returns the top-most value from the priority queue, or NULL if the queue is empty.
+    peek() {
+        const length = this.values_.length;
+        if (!length)
+            throw new Error('You cannot peek from an empty priority queue.');
+
+        return this.values_[length - 1];
+    }
+
+    // Removes the top-most value from the priority queue. Returns the previously top-most value
+    // from the queue, or NULL in case the queue is empty.
+    pop() {
+        if (!this.values_.length)
+            throw new Error('You cannot pop from an empty priority queue.');
+
+        return this.values_.pop();
+    }
+
+    // Returns the number of values that have been added to this queue.
+    size() {
+        return this.values_.length;
+    }
+
+    // Deletes the |value| from the list of values. Has a complexity of O(n). Returns whether the
+    // value(s) have been removed from the priority queue.
+    delete(value) {
+        for (let index = 0; index < this.values_.length; ++index) {
+            if (this.values_[index] !== value)
+                continue;
+
+            let count = 1;
+            for (; index + count < this.values_.length; ++count) {
+                if (this.values_[index + count] !== value)
+                    break;
+            }
+
+            this.values_.splice(index, count);
+            return true;
+        }
+
+        return false;
+    }
+}
 
 exports = PriorityQueue;
--- javascript/base/priority_queue.test.js
+++ javascript/base/priority_queue.test.js
@@ -5,47 +5,135 @@
 const PriorityQueue = require('base/priority_queue.js');
 
 describe('PriorityQueue', it => {
-  it('should behave like a regular queue with a void comparator', assert => {
-    const queue = new PriorityQueue((lhs, rhs) => 1),
-          values = [5, 10, 2, 1];
+    // Comparator that can be used when the order of values does not matter.
+    const staleComparator = (lhs, rhs) => 0;
 
-    values.forEach(value => queue.push(value));
-    values.forEach(value => assert.equal(queue.pop(), value));
-  });
+    // Comparator that can be used to sort values in the priority queue in ascending order.
+    const ascendingComparator = (lhs, rhs) => {
+        if (lhs === rhs)
+            return 0;
 
-  it('should prioritize the items with a ascending comparator', assert => {
-    const queue = new PriorityQueue((lhs, rhs) => lhs > rhs);
+        return lhs > rhs ? 1 : -1;
+    };
 
-    [5, 10, 2, 1].forEach(value => queue.push(value));
-    [1, 2, 5, 10].forEach(expected => {
-      assert.equal(queue.peek(), expected);
-      assert.equal(queue.pop(), expected);
+    // Comparator that can be used to sort values in the priority queue in descending order.
+    const descendingComparator = (lhs, rhs) => {
+        if (lhs === rhs)
+            return 0;
+
+        return lhs > rhs ? -1 : 1;
+    };
+
+    it('should only accept functions as comparators', assert => {
+        assert.throws(() => new PriorityQueue());
+        assert.throws(() => new PriorityQueue(null));
+        assert.throws(() => new PriorityQueue(42));
     });
-  });
 
-  it('should prioritize the items with a descending comparator', assert => {
-    const queue = new PriorityQueue((lhs, rhs) => lhs < rhs);
+    it('should not allow peeking and popping from an empty priority queue', assert => {
+        const queue = new PriorityQueue(staleComparator);
 
-    [5, 10, 2, 1].forEach(value => queue.push(value));
-    [10, 5, 2, 1].forEach(expected => {
-      assert.equal(queue.peek(), expected);
-      assert.equal(queue.pop(), expected);
+        assert.equal(queue.size(), 0);
+        assert.throws(() => queue.peek());
+        assert.throws(() => queue.pop());
     });
-  });
 
-  it('should prioritize items when inserted simultaneously', assert => {
-    const queue = new PriorityQueue((lhs, rhs) => lhs > rhs);
-    queue.push(5, 10, 2, 1);
+    it('should behave like a regular fifo queue with a void comparator', assert => {
+        const queue = new PriorityQueue(staleComparator);
+        const values = [5, 10, 2, 8, 1];
 
-    [1, 2, 5, 10].forEach(expected => assert.equal(queue.pop(), expected));
-  });
+        values.forEach(value => queue.push(value));
+        values.forEach(value => assert.equal(queue.pop(), value));
 
-  it('should be able to count the size of its queue', assert => {
-    const queue = new PriorityQueue((lhs, rhs) => 1);
+        assert.equal(queue.size(), 0);
+    });
 
-    for (let i = 0; i < 100; ++i)
-      queue.push(i);
+    it('should prioritize the items with a ascending comparator', assert => {
+        const queue = new PriorityQueue(ascendingComparator);
 
-    assert.equal(queue.size(), 100);
-  });
+        [5, 10, 8, 2, 1].forEach(value => queue.push(value));
+        [1, 2, 5, 8, 10].forEach(expected => {
+            assert.equal(queue.peek(), expected);
+            assert.equal(queue.pop(), expected);
+        });
+    });
+
+    it('should prioritize the items with a descending comparator', assert => {
+        const queue = new PriorityQueue(descendingComparator);
+
+        assert.equal(queue.size(), 0);
+
+        [5, 10, 2, 1].forEach(value => queue.push(value));
+        [10, 5, 2, 1].forEach(expected => {
+            assert.equal(queue.peek(), expected);
+            assert.equal(queue.pop(), expected);
+        });
+    });
+
+    it('should properly prioritize items when inserted simultaneously', assert => {
+        const queue = new PriorityQueue(ascendingComparator);
+        queue.push(5, 10, 2, 1);
+
+        assert.equal(queue.size(), 4);
+
+        [1, 2, 5, 10].forEach(expected => assert.equal(queue.pop(), expected));
+    });
+
+    it('should be able to count the size of its queue', assert => {
+        const queue = new PriorityQueue(staleComparator);
+
+        for (let i = 0; i < 100; ++i)
+            queue.push(i);
+
+        assert.equal(queue.size(), 100);
+    });
+
+    it('should be able to remove values from the priority queue', assert => {
+        const queue = new PriorityQueue(ascendingComparator);
+        queue.push(5, 10, 8, 2, 8, 1);
+
+        assert.equal(queue.size(), 6);
+        assert.isTrue(queue.delete(8));
+        assert.equal(queue.size(), 4);
+
+        [1, 2, 5, 10].forEach(expected => assert.equal(queue.pop(), expected));
+    });
+
+    it('should work with larger quantities of numbers', assert => {
+        const queue = new PriorityQueue(ascendingComparator);
+        const count = 10000;
+
+        for (let i = 0; i < count; ++i)
+            queue.push(Math.floor(Math.random() * count));
+
+        assert.equal(queue.size(), count);
+
+        let lastNumber = queue.pop();
+        while (queue.size()) {
+            const number = queue.pop();
+
+            assert.isAboveOrEqual(number, lastNumber);
+            lastNumber = number;
+        }
+
+        assert.equal(queue.size(), 0);
+    });
+
+    it('should work with objects rather than numbers as well', assert => {
+        const queue = new PriorityQueue((lhs, rhs) => {
+            if (lhs.value === rhs.value)
+                return 0;
+
+            return lhs.value > rhs.value ? 1 : -1;
+        });
+
+        queue.push({ value: 10000 }, { value: 100 }, { value: 1000 }, { value: 42 });
+
+        assert.equal(queue.size(), 4);
+
+        assert.equal(queue.pop().value, 42);
+        assert.equal(queue.pop().value, 100);
+        assert.equal(queue.pop().value, 1000);
+        assert.equal(queue.pop().value, 10000);
+    });
 });

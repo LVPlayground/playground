80ab8ae1efa234ed250be9b238286b7b64651a8f
Russell Krupke <russell@sa-mp.nl>
2016-05-26 00:59:28 +0100

    Tests for scalar values in Pawn -> JavaScript calls

--- javascript/natives.test.js
+++ javascript/natives.test.js
@@ -9,17 +9,45 @@ describe('Natives', it => {
         assert.throws(() => provideNative('ThisNativeDoesNotExist', '', () => 42));
     });
 
-    it('should be possible to listen to simple zero-argument natives', assert => {
+    it('should throw when providing an invalid signature', assert => {
+        assert.throws(() => provideNative('TestFunction', 'x'));
+    });
+
+    it('should ignore function calls with an invalid number of parameters', assert => {
         let counter = 0;
 
-        provideNative('TestFunction', '', () => ++counter);
+        provideNative('TestFunction', 'i', () => ++counter);
 
-        assert.equal(pawnInvoke('TestFunction'), 1);
-        assert.equal(counter, 1);
+        assert.equal(pawnInvoke('TestFunction'), 0);
+        assert.equal(counter, 0);
     });
 
     it('should be able to return a scalar number in a provided native', assert => {
         provideNative('TestFunction', '', () => 42);
         assert.equal(pawnInvoke('TestFunction'), 42);
+    });
+
+    it('should be able to receive scalar arguments', assert => {
+        provideNative('TestFunction', 'i', value => 2 * value);
+
+        assert.equal(pawnInvoke('TestFunction', 'i', 2), 4);
+        assert.equal(pawnInvoke('TestFunction', 'i', -10), -20);
+
+        provideNative('TestFunction', 'iiiii', (...params) => Math.max(...params));
+        assert.equal(pawnInvoke('TestFunction', 'iiiii', 5, 4, 3, 10, 1), 10);
+
+        provideNative('TestFunction', 'f', value => Math.round(value));
+
+        assert.equal(pawnInvoke('TestFunction', 'f', 12.34), 12);
+        assert.equal(pawnInvoke('TestFunction', 'f', -25.80), -26);
+
+        provideNative('TestFunction', 'ffff', (...params) => Math.round(Math.min(...params)));
+        assert.equal(pawnInvoke('TestFunction', 'ffff', 51.12, 15.551, 5.678, 951.51), 6);
+
+        provideNative('TestFunction', 'iiff', (a, b, c, d) => {
+            return a * c + b * d;
+        });
+
+        assert.equal(pawnInvoke('TestFunction', 'iiff', 5, 3, 10, 5), 5 * 10 + 3 * 5);
     });
 });

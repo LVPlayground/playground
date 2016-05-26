7938ccdecd295fc67212b727ce901d1f5341148d
Russell Krupke <russell@sa-mp.nl>
2016-05-26 11:02:51 +0100

    Remove the EventListener component, and Feature.createEventListener()
    They're not being used.

--- javascript/components/feature_manager/feature.js
+++ javascript/components/feature_manager/feature.js
@@ -2,29 +2,10 @@
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
 
-const EventListener = require('components/events/event_listener.js');
-
 // The Feature class must be the base class of all features.
 class Feature {
   constructor() {
     this.dependencies_ = {};
-    this.eventListeners_ = [];
-  }
-
-  // To be called when the feature shuts down. All known resources associated with the feature will
-  // be disposed and removed from the gamemode as well.
-  dispose() {
-    this.eventListeners_.forEach(listener => listener.dispose());
-    this.eventListeners_ = [];
-  }
-
-  // Creates and returns a new event listener that can be used by this feature. A feature can have
-  // as many event listeners as needed, and usage of these is strongly preferred over global events.
-  createEventListener() {
-    const eventListener = new EventListener();
-    this.eventListeners_.push(eventListener);
-
-    return eventListener;
   }
 
   // Defines a dependency on |featureName|. An exception will be thrown if the dependency could not
@@ -37,6 +18,12 @@ class Feature {
     }
 
     return this.dependencies_[featureName];
+  }
+
+  // To be called when the feature shuts down. All known resources associated with the feature will
+  // be disposed and removed from the gamemode as well.
+  dispose() {
+    // TODO(Russell): Make sure that the dependencies are disposed of first.
   }
 };
 

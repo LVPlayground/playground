c63b8af2fe91e735bd2ef47fd9bfe6cef78763c7
Russell Krupke <russell@sa-mp.nl>
2016-08-02 00:18:20 +0100

    Use the logstash() function

--- javascript/features/logger/log_writer.js
+++ javascript/features/logger/log_writer.js
@@ -2,16 +2,26 @@
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
 
+// Path to the UNIX socket through which logstash events will be reported.
+const LOGSTASH_UNIX_SOCKET = '/tmp/logstash.sock';
+
 // The log writer is responsible for actually writing entries to the backend. It has a mocked
 // implementation used for testing in the test/ directory that should be kept in sync.
 class LogWriter {
+    constructor() {
+        logstash('' /* empty message */, LOGSTASH_UNIX_SOCKET);
+    }
+
     // Writes the |event| to the system log. The timestamp will be appended automatically. Elastic
     // can do this themselves, but that creates a risk of invalid timestamps for delayed imports.
     writeEvent(type, event) {
         event['@timestamp'] = this.createTimestamp();
         event['type'] = type;
 
-        // TODO(Russell): Send the log entry to the backend.
+        const message = JSON.stringify(event).replace(/\\n/g, "\\n")
+                                             .replace(/\\r/g, "\\r");
+
+        logstash(message);
     }
 
     // Creates a string representing the current time as "YYYY-MM-DDTHH:II:SS.UUUZ".

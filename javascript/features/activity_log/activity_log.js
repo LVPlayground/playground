ca4a90a6480fe913ac8c2f71be24760b4358b067
xhoogland <github@xanland.nl>
2016-08-14 02:12:39 +0200

    Nuke the long serial-line. It now only shows the hashed serials still with the IP as we previously had.

--- javascript/features/activity_log/activity_log.js
+++ javascript/features/activity_log/activity_log.js
@@ -96,9 +96,8 @@ class ActivityLog extends Feature {
 
     const numericIpAddress = this.ip2long(player.ipAddress);
     const hashedGpci = Murmur3Hash.generateHash(player.gpci);
-    const bothHashAndNormal = hashedGpci + "|" + player.gpci;
 
-    this.announce_.announceToIRC(JoinIpGpciTag, player.id, player.ipAddress, player.name, bothHashAndNormal);
+    this.announce_.announceToIRC(JoinIpGpciTag, player.id, player.ipAddress, player.name, hashedGpci);
 
     this.recorder_.getIdFromWriteInsertSessionAtConnect(player.name, numericIpAddress, hashedGpci).then (result => {
       this.playerSessionIdMap_.set(player.id, result.sessionId);

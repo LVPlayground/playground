96aa4e00b64448fd0fe22929a18fa5bb2cb7b187
xhoogland <github@xanland.nl>
2016-09-17 19:48:00 +0200

    Change throwing an error at an empty or null-key at generating a Murmur3Hash to just log something to the console. This prevents

--- javascript/features/activity_log/murmur3hash.js
+++ javascript/features/activity_log/murmur3hash.js
@@ -14,8 +14,11 @@ const PrivateSymbol = Symbol('Private method, only Murmur3Hash.generateHash shou
 class Murmur3Hash {
     // Generate our murmur3-hash based on given |key| according to the algorithm
     static generateHash(key) {
-        if (key.trim().length < 1 || key == null)
-            throw new Error('key should not be null or empty.');
+        if (key == null || key.trim().length < 1)
+        {
+            console.log('Error in Murmur3Hash.generateHash(' + key + '): key should not be null or empty.');
+            return 0;
+        }
 
         const remainder = key.length % 4;
         const bytes = key.length - remainder;

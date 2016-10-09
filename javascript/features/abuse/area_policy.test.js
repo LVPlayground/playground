e5fb856a0904a7b260beef5c5c0b733e933c568f
Russell Krupke <russell@sa-mp.nl>
2016-10-09 04:47:46 +0100

    Add very basic AreaPolicy infrastructure

---
+++ javascript/features/abuse/area_policy.test.js
@@ -0,0 +1,20 @@
+// Copyright 2016 Las Venturas Playground. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+const AreaPolicy = require('features/abuse/area_policy.js');
+
+describe('AreaPolicy', it => {
+    it('should return different policies for within and outside of Las Venturas', assert => {
+        const lasVenturasPolicy = AreaPolicy.getForPosition(new Vector(2000, 2000, 0));
+        const sanAndreasPolicy = AreaPolicy.getForPosition(new Vector(0, 0, 0));
+
+        assert.notStrictEqual(lasVenturasPolicy, sanAndreasPolicy);
+
+        assert.isTrue(lasVenturasPolicy.issuingDamageBlocksTeleport);
+        assert.isTrue(sanAndreasPolicy.issuingDamageBlocksTeleport);
+
+        assert.isTrue(lasVenturasPolicy.takingDamageBlocksTeleport);
+        assert.isFalse(sanAndreasPolicy.takingDamageBlocksTeleport);
+    });
+});

935cf39ca5648ec3a33a29707afd6b5dac539143
Russell Krupke <russell@sa-mp.nl>
2016-10-16 04:05:21 +0100

    Revert "Get rid of PacoBeanZ' fench"
    This reverts commit d25626232f04bb18de2753490e0bf21c2addb808.

--- javascript/features/player_favours/player_favours.js
+++ javascript/features/player_favours/player_favours.js
@@ -15,7 +15,6 @@ class PlayerFavours extends Feature {
 
         this.objectRemover_ = new ObjectRemover();
         this.objectRemover_.load('data/favours/caligula_basement_door.json'); // Door which blocks access to basement in caligulas
-        this.objectRemover_.load('data/favours/pacobeanz_fench_ls.json');
 
         this.objectGroups_ = [];
 

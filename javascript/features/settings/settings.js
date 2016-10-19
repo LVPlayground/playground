a4300b6d20b65e89e75c21d93442d91b3449f166
Russell Krupke <russell@sa-mp.nl>
2016-10-19 22:38:10 +0100

    Support change observers in the Settings feature

--- javascript/features/settings/settings.js
+++ javascript/features/settings/settings.js
@@ -12,8 +12,11 @@ class Settings extends Feature {
     constructor() {
         super();
 
-        // Map of setting (as "category/name") to the corresponding Setting instance.
+        // Map of setting identifiers (as "category/name") to the corresponding Setting instance.
         this.settings_ = new Map();
+
+        // Map of setting identifiers to a map of instances to listeners.
+        this.observers_ = new Map();
 
         // TODO: Load setting values that have been overridden from the database.
 
@@ -26,6 +29,45 @@ class Settings extends Feature {
 
     // Gets an iterator with the Settings that are available on Las Venturas Playground.
     getSettings() { return this.settings_.values(); }
+
+    // Adds an observer for the setting |identifier|. It'll be keyed on the |instance|, and the
+    // |listener| will be invoked whenever the value is updated. Will throw on invalid identifiers.
+    //
+    // The listeners are expected to be scoped to the |instance| and have the following prototype:
+    //
+    //     listener(identifier, newValue, oldValue, defaultValue)
+    //
+    // All listeners for the |instance| and |identifier| have to be removed simultaneously.
+    addSettingObserver(identifier, instance, listener) {
+        const setting = this.settings_.get(identifier);
+        if (!setting)
+            throw new Error('Invalid setting given: ' + identifier);
+
+        if (!this.observers_.has(identifier))
+            this.observers_.set(identifier, new Map());
+
+        const observers = this.observers_.get(identifier);
+        if (!observers.has(instance))
+            observers.set(instance, new Set());
+
+        observers.get(instance).add(listener);
+    }
+
+    // Removes the observers for the |identifier| that were keyed on the |instance|. Will throw
+    // when an invalid |identifier| has been passed.
+    removeSettingObserver(identifier, instance) {
+        if (!this.settings_.has(identifier))
+            throw new Error('Invalid setting given: ' + identifier);
+
+        const observers = this.observers_.get(identifier);
+        if (!observers || !observers.has(instance))
+            return;  // there are no listeners for the |identifier|
+
+        observers.delete(instance);
+
+        if (!observers.size)
+            this.observers_.delete(identifier);
+    }
 
     // Gets the value currently assigned to the |identifier|. Will throw when the |identifier| is
     // not known to the settings system, since we can't return anything sensible in that case.
@@ -59,6 +101,15 @@ class Settings extends Feature {
                 throw new Error('Unknown type for the setting ' + identifier + '.');
         }
 
+        // Inform observers for the |identifier| about the value change.
+        const observers = this.observers_.get(identifier);
+        if (observers) {
+            for (const [instance, listeners] of observers) {
+                for (const listener of listeners)
+                    listener.call(instance, identifier, value, setting.value, setting.defaultValue);
+            }
+        }
+
         setting.value = value;
 
         // TODO: Store the |value| in the database, or delete it when it's the same as the default
@@ -67,7 +118,13 @@ class Settings extends Feature {
 
     // ---------------------------------------------------------------------------------------------
 
-    dispose() {}
+    dispose() {
+        for (const setting of this.settings_.values())
+            setting.value = setting.defaultValue;
+
+        this.settings_.clear();
+        this.settings_ = null;
+    }
 }
 
 exports = Settings;
---
+++ javascript/features/settings/settings.test.js
@@ -0,0 +1,112 @@
+// Copyright 2016 Las Venturas Playground. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+describe('Settings', (it, beforeEach) => {
+    let settings = null;
+
+    beforeEach(() => settings = server.featureManager.loadFeature('settings'));
+
+    it('should be able to read settings of different types', assert => {
+        assert.throws(() => settings.getValue());
+        assert.throws(() => settings.getValue('invalid/value'));
+        assert.throws(() => settings.getValue(42));
+
+        const numberValue = settings.getValue('abuse/blocker_damage_issued_time');
+        assert.equal(typeof numberValue, 'number');
+
+        const booleanValue = settings.getValue('abuse/spawn_vehicle_admin_override');
+        assert.equal(typeof booleanValue, 'boolean');
+    });
+
+    it('should be able to update the values of settings', assert => {
+        assert.throws(() => settings.setValue());
+        assert.throws(() => settings.setValue('invalid/value', ''));
+        assert.throws(() => settings.setValue(42, 1337));
+
+        // Numbers
+        {
+            const numberValue = settings.getValue('abuse/blocker_damage_issued_time');
+            assert.equal(typeof numberValue, 'number');
+
+            assert.throws(() => settings.setValue('abuse/blocker_damage_issued_time', true));
+            assert.throws(() => settings.setValue('abuse/blocker_damage_issued_time', 'yesplz'));
+            assert.throws(() => settings.setValue('abuse/blocker_damage_issued_time', [ 123 ]));
+
+            settings.setValue('abuse/blocker_damage_issued_time', numberValue + 42);
+
+            const updatedValue = settings.getValue('abuse/blocker_damage_issued_time');
+            assert.equal(typeof updatedValue, 'number');
+
+            assert.equal(updatedValue, numberValue + 42);
+        }
+
+        // Booleans
+        {
+            const booleanValue = settings.getValue('abuse/spawn_vehicle_admin_override');
+            assert.equal(typeof booleanValue, 'boolean');
+
+            assert.throws(() => settings.setValue('abuse/spawn_vehicle_admin_override', 424));
+            assert.throws(() => settings.setValue('abuse/spawn_vehicle_admin_override', 'yesplz'));
+            assert.throws(() => settings.setValue('abuse/spawn_vehicle_admin_override', [ true ]));
+
+            settings.setValue('abuse/spawn_vehicle_admin_override', !booleanValue);
+
+            const updatedValue = settings.getValue('abuse/spawn_vehicle_admin_override');
+            assert.equal(typeof updatedValue, 'boolean');
+
+            assert.notEqual(updatedValue, booleanValue);
+        }
+    });
+
+    it('should be able to support change observers for the settings', assert => {
+        let invocations = 0;
+
+        let newValue = null;
+        let oldValue = null;
+        let defaultValue = null;
+
+        class MyObserver {
+            onDamageIssuedTimeChange(identifier, inNewValue, inOldValue, inDefaultValue) {
+                assert.equal(identifier, 'abuse/blocker_damage_issued_time');
+
+                ++invocations;
+
+                newValue = inNewValue;
+                oldValue = inOldValue;
+                defaultValue = inDefaultValue;
+            }
+        }
+
+        const observer = new MyObserver();
+
+        settings.addSettingObserver('abuse/blocker_damage_issued_time', observer,
+                                    MyObserver.prototype.onDamageIssuedTimeChange);
+
+        assert.equal(invocations, 0);
+
+        const original = settings.getValue('abuse/blocker_damage_issued_time');
+        assert.equal(typeof original, 'number');
+
+        assert.equal(invocations, 0);
+
+        settings.setValue('abuse/blocker_damage_issued_time', 42);
+
+        assert.equal(invocations, 1);
+        assert.equal(newValue, 42);
+        assert.equal(oldValue, original);
+        assert.equal(defaultValue, original);
+
+        settings.setValue('abuse/blocker_damage_issued_time', 1337);
+
+        assert.equal(invocations, 2);
+        assert.equal(newValue, 1337);
+        assert.equal(oldValue, 42);
+        assert.equal(defaultValue, original);
+
+        settings.removeSettingObserver('abuse/blocker_damage_issued_time', observer);
+        settings.setValue('abuse/blocker_damage_issued_time', original);
+
+        assert.equal(invocations, 2);
+    });
+});

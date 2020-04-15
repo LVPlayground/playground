18fea7e426ef3642b7035d7ddc4badb16127e8fa
Russell <russell@sa-mp.nl>
2020-04-15 23:15:51 +0100

    [NuwaniJS] Improve error reporting for the connection

--- javascript/features/nuwani/runtime/connection.js
+++ javascript/features/nuwani/runtime/connection.js
@@ -7,6 +7,9 @@ import { BackoffPolicy } from 'features/nuwani/runtime/backoff_policy.js';
 // Number of seconds to wait before considering a connection as having failed.
 const kConnectionTimeoutSec = 10;
 
+// Global connection counter allowing log messages to be associated with each other.
+let connectionId = 1;
+
 // The Connection class wraps the actual TCP Socket that's used to establish a connection with
 // the IRC server, and provides functionality for features such as automatic reconnections.
 export class Connection {
@@ -16,6 +19,7 @@ export class Connection {
     backoffPolicy_ = null;
     socket_ = null;
 
+    connection_id_ = null;
     disposed_ = false;
 
     constructor(delegate, servers) {
@@ -27,6 +31,8 @@ export class Connection {
         this.socket_ = new Socket('tcp');
         this.socket_.addEventListener('error', Connection.prototype.onSocketError.bind(this));
         this.socket_.addEventListener('message', Connection.prototype.onSocketMessage.bind(this));
+
+        this.connection_id_ = connectionId++;
     }
     
     // Starts trying to establish a connection with one of the network's servers. Will spin and wait
@@ -34,8 +40,9 @@ export class Connection {
     async connect() {
         let attempt = 0;
 
+        await wait(this.backoffPolicy_.getTimeToNextRequestMs());
+
         do {
-            await wait(this.backoffPolicy_.getTimeToNextRequestMs());
             if (this.disposed_)
                 return;
 
@@ -43,7 +50,9 @@ export class Connection {
             const ip = this.servers_[currentServerIndex].ip;
             const port = this.servers_[currentServerIndex].port;
 
+            this.log(`Connecting to ${ip}:${port}...`);
             const connected = await this.socket_.open(ip, port, kConnectionTimeoutSec);
+
             if (this.disposed_) {
                 if (connected)
                     this.socket_.close();
@@ -57,11 +66,19 @@ export class Connection {
                       : this.backoffPolicy_.markRequestFailed();
 
             if (connected) {
+                this.log(`Connection to ${ip}:${port} succeeded.`);
                 this.delegate_.onConnectionEstablished();
                 return;
             }
 
+            const delay = this.backoffPolicy_.getTimeToNextRequestMs();
+            const delaySec = Math.floor(delay / 1000);
+
+            this.log(`Connection to ${ip}:${port} failed. Next attempt in ${delaySec} seconds.`);
             this.delegate_.onConnectionFailed();
+
+            await wait(delay);
+
             ++attempt;
 
         } while (!this.disposed_);
@@ -70,13 +87,28 @@ export class Connection {
     // Called when an error occurs on the socket that backs the connection. Most errors will require
     // the connection to be closed and reset, which will be initiated by this method.
     onSocketError(event) {
+        this.log(`Error ${event.code}: ${event.message}`);
 
+        // Close the socket if it hasn't closed itself already.
+        if (this.socket_.state !== 'disconnected')
+            this.socket_.close();
+
+        // TODO: Should we automatically reconnect to the server here?
     }
 
     // Called when data has been received from the socket. Each event may contain one or multiple
     // messages that will be forwarded to the Bot.
     onSocketMessage(event) {
+        
+    }
 
+    // Logs the given |message| associated with this connection. The message will not be written to
+    // the console when running tests, to avoid spewing a ton of output.
+    log(message) {
+        if (server.isTest())
+            return;
+        
+        console.log('[Connection:' + this.connection_id_ + '] ' + message);
     }
 
     dispose() {

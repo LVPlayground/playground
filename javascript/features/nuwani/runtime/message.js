1e23011303394587f75b624ab74b48487feaea12
Russell <russell@sa-mp.nl>
2020-04-16 22:35:20 +0100

    [NuwaniJS] Implement a parser for IRC messages

---
+++ javascript/features/nuwani/runtime/message.js
@@ -0,0 +1,104 @@
+// Copyright 2020 Las Venturas Playground. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+import { MessageSource } from 'features/nuwani/runtime/message_source.js';
+
+// Encapsulates a message received. rfc8212 is quite prescriptive in how these can be formatted, but
+// a surprising amount of flexibility is allowed nonetheless. This parser breaks it up in the three
+// key parts: the message source, command and parameters.
+//
+// The implementation follows the following ABNF syntax:
+//
+//   message    =  [ ":" prefix SPACE ] command [ params ] crlf
+//
+//   command    =  1*letter / 3digit
+//   params     =  *14( SPACE middle ) [ SPACE ":" trailing ]
+//              =/  14( SPACE middle ) [ SPACE [ ":" ] trailing ]
+//
+//   middle     =  nospcrlfcl *( ":" / nospcrlfcl )
+//   trailing   =  *( ":" / " " / nospcrlfcl )
+//
+// The ending crlf has already been removed by the Connection class, as messages will be split based
+// on that delimiter. Parsing here is done leniently, to allow for improved compatibility.
+export class Message {
+    source_ = null;
+    command_ = null;
+    params_ = [];
+
+    // Gets the source of this message, if any.
+    get source() { return this.source_; }
+
+    // Gets the command of this message, which is either numeric or textual. The command will always
+    // be returned in uppercase, to avoid the rest of the code from having to think about casing.
+    get command() { return this.command_; }
+
+    // Gets the parameters associated with this message. Always an array.
+    get params() { return this.params_; }
+
+    constructor(message) {
+        if (typeof message !== 'string' || !message.length)
+            throw new Error(`Invalid message given: ${message}`);
+
+        let position = 0;
+
+        // Utility function to skip |position| to the next non-whitespace character. Will return the
+        // |position| for convenient use in method calls.
+        const positionTrimmedForWhitespace = () => {
+            while (position < message.length && message[position] === ' ')
+                ++position;
+            
+            return position;
+        }
+
+        position = this.parseSource(message, positionTrimmedForWhitespace());
+        position = this.parseCommand(message, positionTrimmedForWhitespace());
+
+        while (position < message.length)
+            position = this.parseParam(message, positionTrimmedForWhitespace());
+    }
+
+    // Parses the source of the |message|, starting at the given |position|.
+    parseSource(message, position) {
+        if (message[position] !== ':')
+            return position;
+        
+        const nextSpace = message.indexOf(' ', position);
+        if (nextSpace === -1)
+            throw new Error(`Invalid message given (prefix): ${message}`);
+
+        this.source_ = new MessageSource(message.substring(position + 1, nextSpace));
+        return nextSpace;
+    }
+
+    // Parses the command of the |message|, starting at the given |position|.
+    parseCommand(message, position) {
+        const nextSpace = message.indexOf(' ', position);
+        const command = message.substring(position, nextSpace > position ? nextSpace : undefined);
+
+        if (!/^[a-z0-9]{3,}$/i.test(command))
+            throw new Error(`Invalid message given (command): ${message}`);
+        
+        this.command_ = command.toUpperCase();
+        return nextSpace > position ? nextSpace :
+                                      message.length;
+    }
+
+    // Parses a parameter of the |message|, starting at the given |position|. If the parameter
+    // starts with a colon, the rest of the |message| will be consumed.
+    parseParam(message, position) {
+        if (message[position] === ':') {
+            this.params_.push(message.substring(position + 1).trimEnd());
+            return message.length;
+        }
+
+        const nextSpace = message.indexOf(' ', position);
+        if (nextSpace === -1) {
+            this.params_.push(message.substring(position));
+            return message.length;
+        }
+
+        this.params_.push(message.substring(position, nextSpace));
+        return nextSpace;
+    }
+}
---
+++ javascript/features/nuwani/runtime/message.test.js
@@ -0,0 +1,81 @@
+// Copyright 2020 Las Venturas Playground. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+import { Message } from 'features/nuwani/runtime/message.js';
+
+describe('Message', it => {
+    it('should throw on invalid messages', assert => {
+        assert.throws(() => new Message());
+        assert.throws(() => new Message(''));
+        assert.throws(() => new Message('^%$'));
+        assert.throws(() => new Message(':nickname'));
+    });
+
+    it('should be able to parse messages that are just a command', assert => {
+        const message = new Message('EXAMPLE');
+        assert.isNull(message.source);
+        assert.equal(message.command, 'EXAMPLE');
+        assert.equal(message.params.length, 0);
+    });
+
+    it('should be able to parse messages that do not have a source', assert => {
+        {
+            const message = new Message('COMMAND :foo');
+            assert.isNull(message.source);
+            assert.equal(message.command, 'COMMAND');
+            assert.equal(message.params.length, 1)
+            assert.equal(message.params[0], 'foo');
+        }
+        {
+            const message = new Message('COMMAND #foo bar');
+            assert.isNull(message.source);
+            assert.equal(message.command, 'COMMAND');
+            assert.equal(message.params.length, 2);
+            assert.equal(message.params[0], '#foo');
+            assert.equal(message.params[1], 'bar');
+        }
+    });
+
+    it('should be able to parse numeric commands', assert => {
+        const message = new Message(':server.name 382 :Rehashing...');
+        
+        assert.isNotNull(message.source);
+        assert.isTrue(message.source.isServer());
+        assert.equal(message.source.hostname, 'server.name');
+
+        assert.equal(message.command, '382');
+
+        assert.equal(message.params.length, 1);
+        assert.equal(message.params[0], 'Rehashing...');
+    });
+
+    it('should be able to parse textual commands', assert => {
+        const message = new Message(':nick@host.com privmsg #channel :an irc message');
+
+        assert.isNotNull(message.source);
+        assert.isTrue(message.source.isUser());
+        assert.equal(message.source.nickname, 'nick');
+        assert.equal(message.source.hostname, 'host.com');
+
+        assert.equal(message.command, 'PRIVMSG');
+
+        assert.equal(message.params.length, 2);
+        assert.equal(message.params[0], '#channel');
+        assert.equal(message.params[1], 'an irc message');
+    });
+
+    it('should gracefully deal with excess spacing between parts', assert => {
+        const message = new Message('  :Joe    418   I   :am a teapot  ');
+
+        assert.isNotNull(message.source);
+        assert.isTrue(message.source.isUser());
+        assert.equal(message.source.nickname, 'Joe');
+
+        assert.equal(message.command, '418');
+
+        assert.equal(message.params.length, 2);
+        assert.equal(message.params[0], 'I');
+        assert.equal(message.params[1], 'am a teapot');
+    });
+});

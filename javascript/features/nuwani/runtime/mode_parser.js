348a2487e26d9d58f909bcc254ee8406c91c9e76
Russell <russell@sa-mp.nl>
2020-04-18 01:26:35 +0100

    [NuwaniJS] Implement a parser for the MODE command

--- javascript/features/nuwani/runtime/mode_parser.js
+++ javascript/features/nuwani/runtime/mode_parser.js
@@ -5,18 +5,100 @@
 // Parser to split up the IRC MODE command in a sequence of individual changes. The flexibility
 // offered by the command's syntax makes it undesirable to do this in multiple places.
 export class ModeParser {
+    // The actions that the parser can be in whilst parsing a MODE command.
+    static kActionUndefined = 0;
+    static kActionSet = 1;
+    static kActionUnset = 2;
+
     // Types of modes that will be recognised by the mode parser.
     static kModeWithParameter = 0;
     static kModeWithParameterWhenSet = 1;
     static kModeWithoutParameter = 2;
 
-    modes_ = null;
+    channelModes_ = null;
 
     // Gets the modes that are known to the mode parser.
-    get modes() { return this.modes_; }
+    get channelModes() { return this.channelModes_; }
 
     constructor() {
-        this.modes_ = new Map();
+        this.channelModes_ = new Map();
+    }
+
+    // Parses the given |message|, which must be a MODE command. Returned is a structure in the
+    // following format:
+    //
+    // {
+    //   "set": [
+    //     { "flag": "R" },
+    //     { "flag": "a", "param": "nickname "}
+    //   ],
+    //   "unset": [
+    //     { "flag": "k" }
+    //   ]
+    // }
+    //
+    // Parsing and available flags depend on the type of parsing that has to be done, because
+    // the modes and their arguments are different for users and channels. This can be derived from
+    // the given |message|.
+    parse(message) {
+        if (message.command !== 'MODE')
+            throw new Error('Only MODE command can be parsed.');
+
+        if (message.params.length < 2)
+            throw new Error('Parsing the MODE command requires at least two parameters.')
+
+        const target = message.params[0];
+        const result = {
+            set: [],
+            unset: []
+        };
+
+        let params = message.params.slice(1);
+
+        // This outer loop should only catch actually flag updates, not the parameters that follow
+        // after. Per RFC 8212 it's valid for multiple flag blocks to appear, even though most
+        // popular clients (including mIRC) do not seem to support this.
+        while (params.length > 0) {
+            let action = ModeParser.kActionUndefined;
+            let type, param;
+
+            for (const flag of [...params.shift()]) {
+                if (['+', '-'].includes(flag)) {
+                    action = flag === '-' ? ModeParser.kActionUnset
+                                          : ModeParser.kActionSet;
+
+                    continue;
+                }
+
+                type = this.channelModes_.get(flag);
+                if (type === undefined)
+                    throw new Error('Unrecognized flag in MODE command: ' + flag);
+
+                param = null;
+
+                switch (type) {
+                    case ModeParser.kModeWithParameterWhenSet:
+                        if (action !== ModeParser.kActionSet)
+                            break;
+                        
+                        // deliberate fall-through
+
+                    case ModeParser.kModeWithParameter:
+                        if (!params.length)
+                            throw new Error('Invalid MODE command (missing parameter): ' + message);
+
+                        param = params.shift();
+                        break;
+                }
+
+                if (action === ModeParser.kActionSet)
+                    result.set.push({ flag, param });
+                else
+                    result.unset.push({ flag, param });
+            }
+        }
+
+        return result;
     }
 
     // Sets the channel prefixes for the current network when given by the RPL_ISUPPORT message,
@@ -37,7 +119,7 @@ export class ModeParser {
     //   (d) Modes that never have a parameter.
     //
     // We treat (a) and (b) as the same in our implementation, as no validation will be done. The
-    // modes will be stored in the local |modes_| member.
+    // modes will be stored in the local |channelModes_| member.
     setChannelModes(modes) {
         const groups = modes.split(',');
         if (groups.length != 4)
@@ -52,10 +134,10 @@ export class ModeParser {
     // Sets the |modes| to the given |type|. Only to be used for internal calls.
     setModesWithType(modes, type) {
         for (const mode of [...modes]) {
-            if (this.modes_.has(mode))
+            if (this.channelModes_.has(mode))
                 throw new Error(`The mode "${mode}" has already been registered.`);
             
-            this.modes_.set(mode, type);
+            this.channelModes_.set(mode, type);
         }
     }
 }
--- javascript/features/nuwani/runtime/mode_parser.test.js
+++ javascript/features/nuwani/runtime/mode_parser.test.js
@@ -2,26 +2,128 @@
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
 
+import { Message } from 'features/nuwani/runtime/message.js';
 import { ModeParser } from 'features/nuwani/runtime/mode_parser.js';
 
 describe('ModeParser', it => {
     it('is able to understand both channel modes and prefixes', assert => {
         const parser = new ModeParser();
 
-        assert.equal(parser.modes.size, 0);
+        assert.equal(parser.channelModes.size, 0);
 
         parser.setChannelPrefixes('(Yqaohv)!~&@%+');
         parser.setChannelModes('IXbe,k,FLfjl,ABKMQRSTcimnprstuz');
 
-        assert.equal(parser.modes.size, 34);
+        assert.equal(parser.channelModes.size, 34);
 
         const counts = new Map();
 
-        for (const type of parser.modes.values())
+        for (const type of parser.channelModes.values())
             counts.set(type, (counts.get(type) || 0) + 1);
 
         assert.equal(counts.get(ModeParser.kModeWithParameter), 11);
         assert.equal(counts.get(ModeParser.kModeWithParameterWhenSet), 5);
         assert.equal(counts.get(ModeParser.kModeWithoutParameter), 18);
+    });
+
+    it('should reject invalid MODE commands with an exception', assert => {
+        const parser = new ModeParser();
+
+        assert.throws(() => parser.parse(new Message(':server.name JOIN #echo')));
+        assert.throws(() => parser.parse(new Message(':server.name MODE #echo')));
+    });
+
+    it('is able to parse channel mode changes as defined by the network', assert => {
+        const parser = new ModeParser();
+
+        assert.equal(parser.channelModes.size, 0);
+
+        parser.setChannelPrefixes('(Yqaohv)!~&@%+');
+        parser.setChannelModes('IXbe,k,FLfjl,ABKMQRSTcimnprstuz');
+
+        assert.equal(parser.channelModes.size, 34);
+
+        // Asserts that parsing |modeChanges| will result in the |expected| amendments, which will
+        // be validated in a more structured way, yielding better error messages.
+        const assertParseResult = (modeChanges, expectedChanges) => {
+            const result = parser.parse(new Message(':server.name MODE #echo ' + modeChanges));
+            
+            for (const type of ['set', 'unset']) {
+                const expected = expectedChanges[type];
+                const actual = result[type];
+
+                if (actual.length !== expected.length) {
+                    throw new Error(`Expected ${expected.length} mode flags to be ${type}, but ` +
+                                    `got ${actual.length} instead.`);
+                }
+
+                for (let i = 0; i < actual.length; ++i) {
+                    const expectedMode = expected[i];
+                    const actualMode = actual[i];
+
+                    if (actualMode.flag !== expectedMode.flag) {
+                        throw new Error(`Expected mode ${i} to ${type} to be ` +
+                                        `${expectedMode.flag}, but got ${actualMode.flag} instead`);
+                                            
+                    }
+
+                    if (actualMode.param != expectedMode.param) {
+                        throw new Error(`Expected parameter ${i} to ${type} for flag ` +
+                                        `${expectedMode.flag} to be ${expectedMode.param}, but ` +
+                                        `got ${actualMode.param} instead.`);
+                    }
+                }
+            }
+        }
+        
+        assertParseResult('+c', {
+            set: [{ flag: 'c' }],
+            unset: []
+        });
+
+        assertParseResult('+mn', {
+            set: [{ flag: 'm' }, { flag: 'n' }],
+            unset: []
+        });
+
+        assertParseResult('-stu', {
+            set: [],
+            unset: [{ flag: 's' }, { flag: 't' }, { flag: 'u' }]
+        });
+
+        assertParseResult('+AB-st+Q', {
+            set: [{ flag: 'A' }, { flag: 'B' }, { flag: 'Q' }],
+            unset: [{ flag: 's' }, { flag: 't' }]
+        });
+
+        assertParseResult('+k password', {
+            set: [{ flag: 'k', param: 'password' }],
+            unset: []
+        });
+
+        assertParseResult('+mv-sta+k Joe Ted password', {
+            set: [{ flag: 'm' }, { flag: 'v', param: 'Joe' }, { flag: 'k', param: 'password' }],
+            unset: [{ flag: 's' }, { flag: 't' }, { flag: 'a', param: 'Ted' }]
+        });
+
+        assertParseResult('+c -m +Q', {
+            set: [{ flag: 'c' }, { flag: 'Q' }],
+            unset: [{ flag: 'm' }]
+        });
+
+        assertParseResult('+Ip-v Joe!user@host Ted +i-b banee', {
+            set: [{ flag: 'I', param: 'Joe!user@host' }, { flag: 'p' }, { flag: 'i' }],
+            unset: [{ flag: 'v', param: 'Ted' }, { flag: 'b', param: 'banee' }]
+        });
+
+        assertParseResult('+lL-j 10 5', {
+            set: [{ flag: 'l', param: '10' }, { flag: 'L', param: '5' }],
+            unset: [{ flag: 'j' }]
+        });
+
+        assertParseResult('+j-f+L-l 15 20', {
+            set: [{ flag: 'j', param: '15' }, { flag: 'L', param: '20' }],
+            unset: [{ flag: 'f' }, { flag: 'l' }]
+        });
     });
 });

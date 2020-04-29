1b18c79e2c83f4126bc143ae4e9090cd9fe60d18
Russell <russell@sa-mp.nl>
2020-04-29 01:32:02 +0100

    Implement relative date routines in base/

--- javascript/base/time.js
+++ javascript/base/time.js
@@ -2,10 +2,111 @@
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
 
-// The Time module adds various utility methods to the global object for making timers look nicer
-// when using asynchronous functions.
-
+// Various utility methods (global) for making the `wait()` function easier to deal with.
 global.milliseconds = milliseconds => wait(milliseconds);
 global.seconds = seconds => wait(seconds * 1000);
 global.minutes = minutes => wait(minutes * 60 * 1000);
 global.hours = hours => wait(hours * 60 * 60 * 1000);
+
+// Utility function for formatting "time since", e.g. a difference of 60 seconds ending up being a
+// string that says "1 minute ago". The |suffix| is optional, but included by default.
+export function fromNow({ date, suffix = true } = {}) {
+    return from({ date1: date, date2: new Date(), suffix });
+}
+
+// Utility function that formats the "time since" between |date1| and |date2|. If |date1| happened
+// after |date2|, an exception will be thrown. Use `relativeTime` if you don't care.
+export function from({ date1, date2, suffix = true } = {}) {
+    const relative = relativeTime({ date1, date2 });
+    if (!relative.isPast && !relative.isCurrent)
+        throw new Error('Cannot use `from` and `fromNow` for dates in the future.');
+    
+    if (suffix && !relative.isCurrent)
+        return relative.text + ' ago';
+    else
+        return relative.text;
+}
+
+// Utility function for formatting "time to", e.g. a difference of 60 seconds ending up being a
+// string that says "in 1 minute". The |prefix| is optional, but included by default.
+export function toNow({ date, prefix = true } = {}) {
+    return to({ date1: date, date2: new Date(), prefix });
+}
+
+// Utility function that formats the "time to" between |date1| and |date2|. If |date1| happened
+// before |date2|, an exception will be thrown. Use `relativeTime` if you don't care.
+export function to({ date1, date2, prefix = true } = {}) {
+    const relative = relativeTime({ date1, date2 });
+    if (relative.isPast && !relative.isCurrent)
+        throw new Error('Cannot use `to` and `toNow` for dates in the past.');
+
+    if (prefix && !relative.isCurrent)
+        return 'in ' + relative.text;
+    else
+        return relative.text;
+}
+
+// Calculates the relative time between |date1| and |date2| in textual format. Both of the date
+// arguments should be instances of the JavaScript Date object.
+export function relativeTime({ date1, date2 }) {
+    if (!(date1 instanceof Date) || !(date2 instanceof Date))
+        throw new Error('Input dates must be instances of the JavaScript Date object.');
+
+    const difference = date2.getTime() - date1.getTime();
+    const absoluteDifference = Math.abs(difference);
+    const absoluteDifferenceSeconds = Math.floor(absoluteDifference / 1000);
+
+    // Whether |date1| and |date2| happened at the same moment.
+    const isCurrent = difference === 0;
+
+    // Whether |date1| happened before |date2|.
+    const isPast = difference < 0;
+
+    // (1) Current time and seconds of difference
+    if (absoluteDifferenceSeconds === 0)
+        return { isCurrent, isPast, text: 'now' };
+    else if (absoluteDifferenceSeconds === 1)
+        return { isCurrent, isPast, text: '1 second' };
+    else if (absoluteDifferenceSeconds < 60)
+        return { isCurrent, isPast, text: `${absoluteDifferenceSeconds} seconds` };
+
+    const absoluteDifferenceMinutes = Math.floor(absoluteDifferenceSeconds / 60);
+
+    // (2) Difference in minutes
+    if (absoluteDifferenceMinutes === 1)
+        return { isCurrent, isPast, text: '1 minute' };
+    else if (absoluteDifferenceMinutes < 60)
+        return { isCurrent, isPast, text: `${absoluteDifferenceMinutes} minutes` };
+    
+    const absoluteDifferenceHours = Math.floor(absoluteDifferenceMinutes / 60);
+
+    // (3) Difference in hours
+    if (absoluteDifferenceHours === 1)
+        return { isCurrent, isPast, text: '1 hour' };
+    else if (absoluteDifferenceHours < 24)
+        return { isCurrent, isPast, text: `${absoluteDifferenceHours} hours` };
+    
+    const absoluteDifferenceDays = absoluteDifferenceHours / 24;
+
+    // (4) Difference in days
+    if (absoluteDifferenceDays === 1)
+        return { isCurrent, isPast, text: '1 day' };
+    else if (absoluteDifferenceDays <= 30)
+        return { isCurrent, isPast, text: `${Math.round(absoluteDifferenceDays)} days` };
+    
+    const absoluteDifferenceMonths = Math.max(1, Math.floor(absoluteDifferenceDays / 30.41));
+
+    // (5) Difference in months
+    if (absoluteDifferenceMonths === 1)
+        return { isCurrent, isPast, text: '1 month' };
+    else if (absoluteDifferenceMonths < 12)
+        return { isCurrent, isPast, text: `${absoluteDifferenceMonths} months` };
+
+    const absoluteDifferenceYears = Math.max(1, Math.floor(absoluteDifferenceDays / 365));
+
+    // (6) Difference in years
+    if (absoluteDifferenceYears === 1)
+        return { isCurrent, isPast, text: '1 year' };
+    
+    return { isCurrent, isPast, text: `${absoluteDifferenceYears} years` };
+}
--- javascript/base/time.test.js
+++ javascript/base/time.test.js
@@ -2,6 +2,8 @@
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
 
+import { from, fromNow, to, toNow } from 'base/time.js';
+
 describe('Time', it => {
     it('should add code sugar for millisecond waits', async(assert) => {
         let invoked = false;
@@ -62,5 +64,64 @@ describe('Time', it => {
         await server.clock.advance(2 * 60 * 60 * 1000);
         assert.isTrue(invoked);
     });
-});
 
+    it('should be able to format relative times', assert => {
+        const baseDate = new Date();
+        const baseTime = baseDate.getTime();
+
+        function futureSeconds(sec) { return new Date(baseTime + sec * 1000); }
+        function futureMinutes(min) { return futureSeconds(min * 60); }
+        function futureHours(hours) { return futureMinutes(hours * 60); }
+        function futureDays(days) { return futureHours(days * 24); }
+        function futureMonths(months) { return futureDays(months * 30.42); }
+        function futureYears(years) { return futureDays(years * 365); }
+
+        function pastSeconds(sec) { return new Date(baseTime - sec * 1000); }
+        function pastMinutes(min) { return pastSeconds(min * 60); }
+        function pastHours(hours) { return pastMinutes(hours * 60); }
+        function pastDays(days) { return pastHours(days * 24); }
+        function pastMonths(months) { return pastDays(months * 30.42); }
+        function pastYears(years) { return pastDays(years * 365); }
+
+        assert.throws(() => toNow({ date: futureHours(1) }));
+        assert.throws(() => fromNow({ date: pastHours(1) }));
+
+        assert.equal(to({ date1: baseDate, date2: baseDate }), 'now');
+        assert.equal(to({ date1: baseDate, date2: futureSeconds(1) }), 'in 1 second');
+        assert.equal(to({ date1: baseDate, date2: futureSeconds(2) }), 'in 2 seconds');
+        assert.equal(to({ date1: baseDate, date2: futureSeconds(59) }), 'in 59 seconds');
+        assert.equal(to({ date1: baseDate, date2: futureMinutes(1) }), 'in 1 minute');
+        assert.equal(to({ date1: baseDate, date2: futureMinutes(2) }), 'in 2 minutes');
+        assert.equal(to({ date1: baseDate, date2: futureMinutes(59) }), 'in 59 minutes');
+        assert.equal(to({ date1: baseDate, date2: futureHours(1) }), 'in 1 hour');
+        assert.equal(to({ date1: baseDate, date2: futureHours(2) }), 'in 2 hours');
+        assert.equal(to({ date1: baseDate, date2: futureHours(23) }), 'in 23 hours');
+        assert.equal(to({ date1: baseDate, date2: futureDays(1) }), 'in 1 day');
+        assert.equal(to({ date1: baseDate, date2: futureDays(2) }), 'in 2 days');
+        assert.equal(to({ date1: baseDate, date2: futureDays(30) }), 'in 30 days');
+        assert.equal(to({ date1: baseDate, date2: futureMonths(1) }), 'in 1 month');
+        assert.equal(to({ date1: baseDate, date2: futureMonths(2) }), 'in 2 months');
+        assert.equal(to({ date1: baseDate, date2: futureMonths(11) }), 'in 11 months');
+        assert.equal(to({ date1: baseDate, date2: futureYears(1) }), 'in 1 year');
+        assert.equal(to({ date1: baseDate, date2: futureYears(2) }), 'in 2 years');
+
+        assert.equal(from({ date1: baseDate, date2: baseDate }), 'now');
+        assert.equal(from({ date1: baseDate, date2: pastSeconds(1) }), '1 second ago');
+        assert.equal(from({ date1: baseDate, date2: pastSeconds(2) }), '2 seconds ago');
+        assert.equal(from({ date1: baseDate, date2: pastSeconds(59) }), '59 seconds ago');
+        assert.equal(from({ date1: baseDate, date2: pastMinutes(1) }), '1 minute ago');
+        assert.equal(from({ date1: baseDate, date2: pastMinutes(2) }), '2 minutes ago');
+        assert.equal(from({ date1: baseDate, date2: pastMinutes(59) }), '59 minutes ago');
+        assert.equal(from({ date1: baseDate, date2: pastHours(1) }), '1 hour ago');
+        assert.equal(from({ date1: baseDate, date2: pastHours(2) }), '2 hours ago');
+        assert.equal(from({ date1: baseDate, date2: pastHours(23) }), '23 hours ago');
+        assert.equal(from({ date1: baseDate, date2: pastDays(1) }), '1 day ago');
+        assert.equal(from({ date1: baseDate, date2: pastDays(2) }), '2 days ago');
+        assert.equal(from({ date1: baseDate, date2: pastDays(30) }), '30 days ago');
+        assert.equal(from({ date1: baseDate, date2: pastMonths(1) }), '1 month ago');
+        assert.equal(from({ date1: baseDate, date2: pastMonths(2) }), '2 months ago');
+        assert.equal(from({ date1: baseDate, date2: pastMonths(11) }), '11 months ago');
+        assert.equal(from({ date1: baseDate, date2: pastYears(1) }), '1 year ago');
+        assert.equal(from({ date1: baseDate, date2: pastYears(2) }), '2 years ago');
+    });
+});

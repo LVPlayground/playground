1979b19aae87674ed6dd6b1e16c5774d1ec1f909
Russell <russell@sa-mp.nl>
2020-05-12 01:11:24 +0100

    Implement /mute, /muted and /unmute in JavaScript

--- javascript/features/communication_commands/communication_commands.js
+++ javascript/features/communication_commands/communication_commands.js
@@ -5,6 +5,8 @@
 import CommandBuilder from 'components/command_manager/command_builder.js';
 import Feature from 'components/feature_manager/feature.js';
 
+import { relativeTime } from 'base/time.js';
+
 // Provides a series of commands associated with communication on Las Venturas Playground. These
 // commands directly serve the Communication feature, but require a dependency on the `announce`
 // feature which is prohibited given that Communication is a foundational feature.
@@ -58,7 +60,32 @@ export default class CommunicationCommands extends Feature {
     // Mutes the given player for the indicated amount of time, in minutes. This will cut off all
     // ways of communication for them, except for administrator chat.
     onMuteCommand(player, targetPlayer, duration = 3) {
+        const timeRemaining = this.muteManager.getPlayerRemainingMuteTime(targetPlayer);
 
+        const proposedTime = duration * 60;
+        const proposedText = relativeTime({
+            date1: new Date(),
+            date2: new Date(Date.now() + proposedTime * 1000 )
+        }).text;
+
+        this.muteManager.mutePlayer(targetPlayer, proposedTime);
+
+        if (!timeRemaining) {
+            targetPlayer.sendMessage(
+                Message.MUTE_MUTED_TARGET, player.name, player.id, proposedText);
+        } else {
+            const change = timeRemaining >= proposedTime ? 'decreased' : 'extended';
+
+            // Inform the |targetPlayer| about their punishment having changed.
+            targetPlayer.sendMessage(
+                Message.MUTE_MUTED_TARGET_UPDATE, player.name, player.id, change, proposedText);
+        }
+
+        this.announce_().announceToAdministrators(
+            Message.MUTE_ADMIN_MUTED, player.name, player.id, targetPlayer.name, targetPlayer.id,
+            proposedText);
+
+        player.sendMessage(Message.MUTE_MUTED, targetPlayer.name, targetPlayer.id, proposedText);
     }
 
     // /muteall [on/off]
@@ -74,7 +101,25 @@ export default class CommunicationCommands extends Feature {
     // Displays an overview of the currently muted players to |player|, and how much time they have
     // left before they automatically get unmuted.
     onMutedCommand(player) {
+        let hasMutedPlayers = false;
 
+        for (const otherPlayer of server.playerManager) {
+            const remainingTime = this.muteManager.getPlayerRemainingMuteTime(otherPlayer);
+            if (!remainingTime)
+                continue;
+
+            hasMutedPlayers = true;
+
+            const duration = relativeTime({
+                date1: new Date(),
+                date2: new Date(Date.now() + remainingTime * 1000 )
+            }).text;
+
+            player.sendMessage(Message.MUTE_MUTED_LIST, otherPlayer.name, otherPlayer.id, duration);
+        }
+
+        if (!hasMutedPlayers)
+            player.sendMessage(Message.MUTE_MUTED_NOBODY);
     }
 
     // /showreport [player]
@@ -90,7 +135,19 @@ export default class CommunicationCommands extends Feature {
     // Immediately unmutes the given player for an unspecified reason. They'll be able to use all
     // forms of communication on the server again.
     onUnmuteCommand(player, targetPlayer) {
+        const timeRemaining = this.muteManager.getPlayerRemainingMuteTime(targetPlayer);
+        if (!timeRemaining) {
+            player.sendMessage(Message.MUTE_UNMUTE_NOT_MUTED, targetPlayer.name, targetPlayer.id);
+            return;
+        }
 
+        this.muteManager.unmutePlayer(targetPlayer);
+
+        this.announce_().announceToAdministrators(
+            Message.MUTE_ADMIN_UNMUTED, player.name, player.id, targetPlayer.name, targetPlayer.id);
+
+        targetPlayer.sendMessage(Message.MUTE_UNMUTED_TARGET, player.name, player.id);
+        player.sendMessage(Message.MUTE_UNMUTED, targetPlayer.name, targetPlayer.id);
     }
 
     dispose() {

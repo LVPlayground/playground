e02af325475f21f02e7157410999024a88500299
Russell <russell@sa-mp.nl>
2020-05-18 14:58:33 +0100

    Include the Haystack code that doesn't do anything yet

---
+++ javascript/features/haystack/haystack.js
@@ -0,0 +1,36 @@
+// Copyright 2020 Las Venturas Playground. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+import Feature from 'components/feature_manager/feature.js';
+import { HaystackGame } from 'features/haystack/haystack_game.js';
+
+// Provides the haystack game, in which players have to climb a series of hay stacks to reach the
+// top of the tower. Implemented on top of the Games infrastructure.
+export default class Haystack extends Feature {
+    games_ = null;
+
+    constructor() {
+        super();
+
+        // The Haystack feature is a game, and will register itself as such.
+        this.games_ = this.defineDependency('games');
+        this.games_.addReloadObserver(this, () => this.initialize());
+
+        this.initialize();
+    }
+
+    // Initializes the Haystack game by registering the game with the Games runtime.
+    initialize() {
+        this.games_().registerGame(HaystackGame, {
+            name: 'Haystack',
+            command: 'newhaystack',
+            minimumPlayers: 1,
+            maximumPlayers: 20,
+        });
+    }
+
+    dispose() {
+        this.games_().removeGame(HaystackGame);
+    }
+}
---
+++ javascript/features/haystack/haystack_game.js
@@ -0,0 +1,11 @@
+// Copyright 2020 Las Venturas Playground. All rights reserved.
+// Use of this source code is governed by the MIT license, a copy of which can
+// be found in the LICENSE file.
+
+import { Game } from 'features/games/game.js';
+
+// Implementation of the Game class specifically for the Haystack minigame. An instance of this
+// class will be created by the Games infrastructure when it's started.
+export class HaystackGame extends Game {
+
+}

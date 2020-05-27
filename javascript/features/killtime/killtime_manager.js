f6e1e201bd7a529ab91f9b98a3d89ee2f2ce1437
OttoRocket-LVP <onnoajax+lvp@gmail.com>
2020-05-28 00:25:51 +0200

    remove possible spawn weapons

--- javascript/features/killtime/killtime_manager.js
+++ javascript/features/killtime/killtime_manager.js
@@ -25,8 +25,7 @@ class KilltimeManager {
     }
 
     get validWeapons() { 
-        return [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 22, 23, 24, 25, 26, 27, 28, 
-                29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 42];
+        return [2, 3, 5, 6, 7, 10, 11, 12, 13, 14, 15, 18, 33, 35, 36, 37, 38, 39, 42];
     }
 
     // Sets the killtime running for |minutes|, initializes the map for the scores and announces the killtime-start.

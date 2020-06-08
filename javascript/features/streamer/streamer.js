52ad151f14b795e9ced38581c991d44598d461d3
Russell <russell@sa-mp.nl>
2020-06-08 03:53:23 +0100

    Delete the vehicle streamer

--- javascript/features/streamer/streamer.js
+++ javascript/features/streamer/streamer.js
@@ -1,52 +1,44 @@
-// Copyright 2016 Las Venturas Playground. All rights reserved.
+// Copyright 2020 Las Venturas Playground. All rights reserved.
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
 
 import Feature from 'components/feature_manager/feature.js';
-import Scheduler from 'features/streamer/scheduler.js';
-import VehicleStreamer from 'features/streamer/vehicle_streamer.js';
 
-// There are various limits in both GTA: San Andreas and San Andreas: Multiplayer that limit the
-// amount of things we can do in Las Venturas Playground. We implement our own set of streamers for
-// that reason, enabling features to go beyond these limits if they must.
-class Streamer extends Feature {
+import { StreamableVehicleInfo } from 'features/streamer/streamable_vehicle_info.js';
+import { StreamableVehicle } from 'features/streamer/streamable_vehicle.js';
+
+// Enhances Las Venturas Playground with the ability to exceed the default vehicle limits. All
+// vehicles part of freeroam, houses and similar features should be created through the streamer.
+export default class Streamer extends Feature {
     constructor() {
         super();
-
-        // The streaming distance that will apply to all streamers on LVP.
-        const streamingDistance = 300 /* units */;
-
-        this.vehicleStreamer_ = new VehicleStreamer({
-            maxVisible: 1000 /* max vehicles */,
-            streamingDistance
-        });
-
-        this.scheduler_ = new Scheduler(this.vehicleStreamer_);
-        this.scheduler_.addStreamer(this.vehicleStreamer_);
-
-        // Starts running the scheduler until this feature gets disposed of.
-        this.scheduler_.start();
     }
 
     // ---------------------------------------------------------------------------------------------
-    // Public API of the Streamer feature.
+    // Public API of the Streamer feature
     // ---------------------------------------------------------------------------------------------
 
-    // Returns the vehicle streamer for Las Venturas Playground. Note that the vehicle streamer is
-    // only responsible for vehicles created in the main virtual world, in the main interior.
-    getVehicleStreamer() {
-        return this.vehicleStreamer_;
+    // Creates a new streamable vehicle on the server. The |vehicle| must thus follow the syntax of
+    // the StreamableVehicleInfo object. An instance of StreamableVehicle will be returned.
+    createVehicle(vehicleInfo) {
+        if (!(vehicleInfo instanceof StreamableVehicleInfo))
+            throw new Error(`The vehicle info must be given as a StreamableVehicleInfo instance.`);
+        
+        // TODO: Implement this method
+
+        return new StreamableVehicle(vehicleInfo);
+    }
+
+    // Deletes the given |vehicle| from the server, which must be a StreamableVehicle instance. Only
+    // the feature that created the vehicle should be removing it.
+    deleteVehicle(vehicle) {
+        if (!(vehicle instanceof StreamableVehicle))
+            throw new Error(`The vehicle must be given as a StreamableVehicle instance.`);
+        
+        // TODO: Implement this method
     }
 
     // ---------------------------------------------------------------------------------------------
 
-    dispose() {
-        this.scheduler_.dispose();
-        this.scheduler_ = null;
-
-        this.vehicleStreamer_.dispose();
-        this.vehicleStreamer_ = null;
-    }
+    dispose() {}
 }
-
-export default Streamer;

6c07e2dbfc9d1169c5771702871ddf6a6791a0c8
Russell <russell@sa-mp.nl>
2020-06-13 23:36:48 +0100

    Store session statistics in the `sessions` table

--- javascript/features/activity_log/activity_log.js
+++ javascript/features/activity_log/activity_log.js
@@ -93,7 +93,7 @@ export default class ActivityLog extends Feature {
         if (!sessionId)
             return;  // no session is known for the given |player|
         
-        this.recorder_.finalizePlayerSession(sessionId);
+        this.recorder_.finalizePlayerSession(player, sessionId);
         this.sessionIds_.delete(player);
     }
 

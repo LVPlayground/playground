8255dde2fb6f4d23eb2a384a399b21d9d65d1d81
Russell <russell@sa-mp.nl>
2020-06-19 17:02:40 +0100

    Fix more tests

--- javascript/features/player_commands/player_commands.js
+++ javascript/features/player_commands/player_commands.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
 
-import Feature from 'components/feature_manager/feature.js';
+import { Feature } from 'components/feature_manager/feature.js';
 import { PlayerCommandsCommands } from 'features/player_commands/player_commands_commands.js';
 
 export default class PlayerCommands extends Feature {
--- javascript/features/player_commands/player_commands_commands.test.js
+++ javascript/features/player_commands/player_commands_commands.test.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
 
-import MockPawnInvoke from 'base/test/mock_pawn_invoke.js';
+import { MockPawnInvoke } from 'base/test/mock_pawn_invoke.js';
 
 describe('PlaygroundCommands', (it, beforeEach) => {
     let commands = null;

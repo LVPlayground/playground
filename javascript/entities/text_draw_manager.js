d1988f4e0c47d9dd3c45b1459500667693c5be17
Russell <russell@sa-mp.nl>
2020-08-06 23:02:28 +0100

    Introduce a TextDraw entity in //entities

--- javascript/entities/text_draw_manager.js
+++ javascript/entities/text_draw_manager.js
@@ -2,6 +2,11 @@
 // Use of this source code is governed by the MIT license, a copy of which can
 // be found in the LICENSE file.
 
+import { PlayerTextDrawNatives, TextDrawNatives } from 'entities/text_draw_natives.js';
+import { TextDraw } from 'entities/text_draw.js';
+
+import { MockTextDrawNatives } from 'entities/text_draw_natives.js';
+
 // Manages the text draws that have been created on the server, both per-player text draws and ones
 // that are global to all players. Text Draws can be created by the ScopedEntities class too.
 export class TextDrawManager {
@@ -15,6 +20,45 @@ export class TextDrawManager {
 
     // Gets the total number of text draws that has been created on the server.
     get count() { return this.#globalTextDraws_.size + this.#playerTextDraws_.size; }
+
+    // Creates a text draw on the server. The position and text are required, all other values are
+    // optional. When a |player| is given, a per-player text draw will be created, otherwise a
+    // global text draw will be preferred instead.
+    createTextDraw({ position, text, player = null, alignment = null, backgroundColor = null,
+                     box = null, boxColor = null, color = null, font = null, letterSize = null,
+                     outline = null, previewModel = null, previewRotation = null,
+                     previewScale = null, previewVehicleColor = null, proportional = null,
+                     selectable = null, shadow = null, textSize = null } = {}) {
+        let natives = null;
+
+        // Determine on the scope of the text draw, which defines how we'll create it and interact
+        // with it. Mocked natives will always be used when running tests.
+        if (server.isTest())
+            natives = new MockTextDrawNatives();
+        else if (player !== null)
+            natives = new PlayerTextDrawNatives(player);
+        else
+            natives = new TextDrawNatives();
+
+        const textDraw = new TextDraw(this, natives);
+
+        // Initialize the text draw, with all available settings. This will create it on the server.
+        textDraw.initialize(position, text, {
+            alignment, backgroundColor, box, boxColor, color, font, letterSize, outline,
+            previewModel, previewRotation, previewScale, previewVehicleColor, proportional,
+            selectable, shadow, textSize
+        });
+
+        // Depending on whether a |player| was given, store it in the global or in the player bucket
+        // in our internal storage. This might make mapping easier when necessary.
+        if (player !== null)
+            this.#playerTextDraws_.add(textDraw);
+        else
+            this.#globalTextDraws_.add(textDraw);
+
+        // Return the text draw so that the caller can do whatever they want with it.
+        return textDraw;
+    }
 
     // Removes the |textDraw| from the maintained set of text draws. Should only be used by the
     // TextDraw implementation to inform the manager about their disposal.
